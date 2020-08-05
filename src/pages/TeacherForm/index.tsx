d63d11a2d0942ad3332fe327a73fc52192988f01
Maycon Gorgonha <maycon.gorgonha@hotmail.com>
2020-08-05 18:10:53 -0300

    add description prop

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -6,7 +6,11 @@ import PageHeader from '../../components/PageHeader';
 const TeacherForm = (): JSX.Element => {
   return (
     <div id="page-teacher-form" className="container">
-      <PageHeader title="Que incrível que você quer dar aulas!" />
+      <PageHeader
+        title="Que incrível que você quer dar aulas!"
+        description="O primeiro passo, é preencher esse
+        formulário de inscrição."
+      />
     </div>
   );
 };
