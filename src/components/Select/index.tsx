ca07cbcdc3408f857ebcb55ea36a5feb6133d385
Maycon Gorgonha <maycon.gorgonha@hotmail.com>
2020-08-05 22:13:45 -0300

    create select component

---
+++ src/components/Select/index.tsx
@@ -0,0 +1,39 @@
+import React, { InputHTMLAttributes } from 'react';
+
+import './styles.css';
+
+interface SelectProps extends InputHTMLAttributes<HTMLSelectElement> {
+  label: string;
+  name: string;
+  options: Array<{
+    value: string;
+    label: string;
+  }>;
+}
+
+const Select: React.FC<SelectProps> = ({
+  label,
+  name,
+  options,
+  ...rest
+}): JSX.Element => {
+  return (
+    <div className="select-block">
+      <label htmlFor={name}>
+        {label}
+        <select defaultValue="" id={name} {...rest}>
+          <option value="" disabled hidden>
+            Selecione uma opção
+          </option>
+          {options.map(option => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+      </label>
+    </div>
+  );
+};
+
+export default Select;
