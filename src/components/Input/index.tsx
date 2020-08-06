3da556853a5b892c2caa0f985a1a0dd57d739ea9
Maycon Gorgonha <maycon.gorgonha@hotmail.com>
2020-08-05 22:13:32 -0300

    create input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,21 @@
+import React, { InputHTMLAttributes } from 'react';
+
+import './styles.css';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  name: string;
+}
+
+const Input: React.FC<InputProps> = ({ label, name, ...rest }): JSX.Element => {
+  return (
+    <div className="input-block">
+      <label htmlFor={name}>
+        {label}
+        <input type="text" id={name} {...rest} />
+      </label>
+    </div>
+  );
+};
+
+export default Input;
