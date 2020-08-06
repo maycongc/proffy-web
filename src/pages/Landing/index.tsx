90bb3ffdd2b78418277e4d791e4b601fb3936c95
Maycon Gorgonha <maycon.gorgonha@hotmail.com>
2020-08-05 22:35:13 -0300

    add landing connection to api

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
@@ -9,8 +9,18 @@ import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
 const Landing = (): JSX.Element => {
+  const [totalConnections, setTotalConnections] = useState<number>(0);
+
+  useEffect(() => {
+    api.get('/connections').then(res => {
+      const { total } = res.data;
+      setTotalConnections(total);
+    });
+  }, []);
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="">
@@ -38,7 +48,7 @@ const Landing = (): JSX.Element => {
         </div>
 
         <span className="total-connections">
-          Total de 200 conexões já realizadas
+          Total de {totalConnections} conexões já realizadas
           <img src={purpleHeartIcon} alt="Coração roxo" />
         </span>
       </div>
