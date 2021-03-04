d138e5c40eb6769386c9d417898c5b94a34f3971
Maycon Gorgonha <maycon.gorgonha@gmail.com>
2021-03-04 01:07:10 -0300

    Updates

---
+++ precache-manifest.c1f4e34ea441f01ef2f2514a19244430.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "140e134e1d99a1e78e44891f02b1be39",
+    "url": "/proffy-web/index.html"
+  },
+  {
+    "revision": "02de2c9be3d6759abcfe",
+    "url": "/proffy-web/static/css/main.57e83155.chunk.css"
+  },
+  {
+    "revision": "c5ee115b265daaae20f0",
+    "url": "/proffy-web/static/js/2.1a9cf383.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/proffy-web/static/js/2.1a9cf383.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02de2c9be3d6759abcfe",
+    "url": "/proffy-web/static/js/main.b04102c5.chunk.js"
+  },
+  {
+    "revision": "933dee687ea3346c9d7e",
+    "url": "/proffy-web/static/js/runtime-main.faf84c64.js"
+  },
+  {
+    "revision": "1fbb1f8bd147351e311921cdf4e80dc3",
+    "url": "/proffy-web/static/media/back.1fbb1f8b.svg"
+  },
+  {
+    "revision": "9ed76be57e0f41fb4092cbbcb15e0875",
+    "url": "/proffy-web/static/media/give-classes.9ed76be5.svg"
+  },
+  {
+    "revision": "c86272eb642793deb29b7eec180c05a5",
+    "url": "/proffy-web/static/media/landing.c86272eb.svg"
+  },
+  {
+    "revision": "1f9a96a14f86a1ccd682159fe038774d",
+    "url": "/proffy-web/static/media/logo.1f9a96a1.svg"
+  },
+  {
+    "revision": "e042bb237d1a08daabd12bf0a4fb9780",
+    "url": "/proffy-web/static/media/purple-heart.e042bb23.svg"
+  },
+  {
+    "revision": "3c22fb01ba7434dfa52b083a67158019",
+    "url": "/proffy-web/static/media/study.3c22fb01.svg"
+  },
+  {
+    "revision": "744fb7ba0c3df74462352a3858badeb6",
+    "url": "/proffy-web/static/media/warning.744fb7ba.svg"
+  },
+  {
+    "revision": "c2ffa8a6a2f6690a5eba826c625715ab",
+    "url": "/proffy-web/static/media/whatsapp.c2ffa8a6.svg"
+  }
+]);
